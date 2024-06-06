a35287d08b3fdd27733c1682923738f49edd51f4
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-06 11:11:41 -0400

    Clone from Allocate On 7th (2a19d5d0-5d3d-4fb3-b0f5-02f9d6fb4acf)

---
+++ 03faeeef-0cd1-4fa1-843c-7af2c519015b/main.py
@@ -0,0 +1,28 @@
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from datetime import datetime
+
+class TradingStrategy(Strategy):
+
+   def __init__(self):
+      self.tickers = ["NEE", "TSLA", "VWDRY", "ENPH", "DNNGY", "FSLR", "SEDG", "CSIQ", "IBDRY", "XEL", "ECL"]
+      self.weights = [0.12, 0.12 , 0.1 , 0.1, 0.1, 0.1, 0.08,0.08, 0.08, 0.06, 0.06]
+      self.equal_weighting = False
+
+   @property
+   def interval(self):
+      return "1day"
+
+   @property
+   def assets(self):
+      return self.tickers
+
+   def run(self, data):
+      today = datetime.now()
+      if today.day == 7:
+         if self.equal_weighting: 
+            allocation_dict = {ticker: 1/len(self.tickers) for ticker in self.tickers}
+         else:
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+         return TargetAllocation(allocation_dict)
+      return None
