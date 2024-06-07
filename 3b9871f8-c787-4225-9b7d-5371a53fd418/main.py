8667553c937e8911b98f0e060ca934b570aa4e81
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-06-07 11:47:48 -0400

    User Justin Graves saved changes to multiple files files at 06/07/2024, 15:47:47 UTC time.

--- 3b9871f8-c787-4225-9b7d-5371a53fd418/main.py
+++ 3b9871f8-c787-4225-9b7d-5371a53fd418/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.logging import log
+from datetime import datetime
+
+class TradingStrategy(Strategy):
+
+   def __init__(self):
+      self.tickers = ["NVDA", "AMD", "GOOGL", "MSFT", "AMZN", "IBM", "INTC", "MU", "QCOM", "TXN", "AVGO", "AMAT", "NXPI", "SWKS", "LRCX", "ADI", "KLAC", "MRVL", "CDNS", "SNPS", "PANW", "FTNT", "CHKP", "OKTA", "NET", "NOW", "VMW", "DOCU", "TEAM", "TWLO"]
+      self.weights = [0.08, 0.08, 0.07, 0.07, 0.06, 0.06, 0.06, 0.05, 0.05, 0.03, 0.03, 0.03, 0.025, 0.025, 0.02, 0.02, 0.015, 0.015, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01, 0.01]
+      self.equal_weighting = False
+
+   @property
+   def interval(self):
+      return "1day"
+
+   @property
+   def assets(self):
+      return self.tickers
+
+   def run(self, data):
+      today = datetime.strptime(str(next(iter(data['ohlcv'][-1].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      yesterday = datetime.strptime(str(next(iter(data['ohlcv'][-2].values()))['date']), '%Y-%m-%d %H:%M:%S')
+      
+      if today.day == 12 or (today.day > 12 and yesterday.day < 12):
+         if self.equal_weighting: 
+            allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+         else:
+            allocation_dict = {self.tickers[i]: self.weights[i] for i in range(len(self.tickers))} 
+         return TargetAllocation(allocation_dict)
+      return None
