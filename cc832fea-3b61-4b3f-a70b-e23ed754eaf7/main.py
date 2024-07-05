b0435c73b8400763b2e939dec0bcc04e4d218e75
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-05 12:57:00 -0400

    User Justin Graves saved changes to multiple files files at 07/05/2024, 16:57:00 UTC time.

--- cc832fea-3b61-4b3f-a70b-e23ed754eaf7/main.py
+++ cc832fea-3b61-4b3f-a70b-e23ed754eaf7/main.py
@@ -1,1 +1,28 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA, MACD, RSI
+from surmount.logging import log
+from surmount.data import Asset 
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self._assets = ['SPY']
+
+    @property
+    def assets(self):
+        return self._assets
+
+    @property 
+    def interval(self):
+        return "1day"
+
+    def run(self, data):
+        return TargetAllocation({'GME': 1})
+
+from datetime import datetime
+
+start = datetime.strptime("2022-11-10", '%Y-%m-%d')
+end = datetime.strptime("2022-11-16", '%Y-%m-%d')
+a = backtest(TradingStrategy(), start, end, 1000)
+
+print(a['stats'])
