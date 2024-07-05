242d1c776f89aa2564c8144fead03eaa8725a886
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-07-05 14:50:46 -0400

    User Justin Graves saved changes to multiple files files at 07/05/2024, 18:50:45 UTC time.

--- cc832fea-3b61-4b3f-a70b-e23ed754eaf7/main.py
+++ cc832fea-3b61-4b3f-a70b-e23ed754eaf7/main.py
@@ -6,7 +6,7 @@ from surmount.data import Asset
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self._assets = ['SPY']
+        self._assets = ['SPY', 'GME']
 
     @property
     def assets(self):
